b32d5fcbbd1b671c9217e80e49a78e891adff4e1
Alximik <alosasinkaruk@gmail.com>
2025-05-29 16:34:52 +0300

    Update Ver.0.0.4
    Added new :
    - 11 Tracks for acoustic guitar
    - 1 Tracks for electric guitar
    
    Reduced weight of the mod: 115 > 19 mb
    
    Updated image and description of the mod
    
    Bugfixes:
    
    - Fixed micro-stuttering when selecting a track
    - Fixed “Error: missing translation” when rendering addon text

---
+++ Contents/mods/NieR Replicant Automata Music/media/lua/client/TimedActions/PlayAcousticGuitarTracks_RAD.lua
@@ -0,0 +1,25 @@
+--Acoustic Guitar Tracks
+local GuitarAcousticTracks = require "TimedActions/PlayGuitarAcousticTracks"
+
+		table.insert(GuitarAcousticTracks, {level=0, sound="Acoustic Guitar_00_Drakengard 3_Aethervox (Empty Tone)", length=220, name="ContextMenu_00_01_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=0, sound="Acoustic Guitar_00_NieR Replicant_ Kaine's theme (trailer edition)", length=75, name="ContextMenu_00_02_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=1, sound="Acoustic Guitar_01_NieR Automata_The Tower", length=217, name="ContextMenu_01_01_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=1, sound="Acoustic Guitar_01_Nier Replicant Ver1.22_Halua Emil", length=154, name="ContextMenu_01_02_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=2, sound="Acoustic Guitar_02_NieR Automata Ver 1.1a_OP escalate", length=137, name="ContextMenu_02_01_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=2, sound="Acoustic Guitar_02_Nier Replicant Ver1.22_Dispossession", length=202, name="ContextMenu_02_02_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=3, sound="Acoustic Guitar_03_NieR Automata_VAG_RADue Hope", length=216, name="ContextMenu_03_01_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=4, sound="Acoustic Guitar_04_NieR Automata_Peaceful Sleep", length=140, name="ContextMenu_04_01_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=4, sound="Acoustic Guitar_04_Nier Replicant Ver1.22_Kainé Salvation", length=217, name="ContextMenu_04_02_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=5, sound="Acoustic Guitar_05_Drakengard 3_Black Song (Kuroi Uta)", length=213, name="ContextMenu_05_01_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=5, sound="Acoustic Guitar_05_NieR Replicant_Yonah Ashes of Dreams", length=181, name="ContextMenu_05_02_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=6, sound="Acoustic Guitar_06_NieR Automata_A Beautiful Song", length=221, name="ContextMenu_06_01_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=6, sound="Acoustic Guitar_06_NieR Automata_City Ruin", length=242, name="ContextMenu_06_02_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=6, sound="Acoustic Guitar_06_Nier Replicant Ver1.22_Shadowlord", length=220, name="ContextMenu_06_03_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=7, sound="Acoustic Guitar_07_NieR Automata_Amusement Park", length=180, name="ContextMenu_07_01_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=7, sound="Acoustic Guitar_07_NieR Automata_Dependent Weakling (Eve Battle Theme)", length=273, name="ContextMenu_07_02_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=7, sound="Acoustic Guitar_07_Nier Replicant Ver1.22_Fleeting Words (Mermaid Louise theme)", length=293, name="ContextMenu_07_03_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=8, sound="Acoustic Guitar_08_NieR Automata_Song of the Ancients_Atonement", length=260, name="ContextMenu_08_01_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=8, sound="Acoustic Guitar_08_Nier Replicant Ver1.22_Grandma", length=214, name="ContextMenu_08_02_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=9, sound="Acoustic Guitar_09_NieR Automata_Voice of No Return Hollow Dreams", length=139, name="ContextMenu_09_01_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=9, sound="Acoustic Guitar_09_NieR Replicant_The Wretched Automations", length=247, name="ContextMenu_09_02_AG_RAD"});
+		table.insert(GuitarAcousticTracks, {level=10, sound="Acoustic Guitar_10_NieR Automata_Weight of the World", length=360, name="ContextMenu_10_01_AG_RAD"});
---
+++ Contents/mods/NieR Replicant Automata Music/media/lua/client/TimedActions/PlayElectricGuitarTracks__RAD.lua
@@ -0,0 +1,4 @@
+-- Electric Guitar Tracks
+local GuitarElectricTracks = require "TimedActions/PlayGuitarElectricTracks"
+
+		table.insert(GuitarElectricTracks, {level=5, sound="Electric Guitar_05_NieR Automata_Song of Ancients Devola", length=148, name="ContextMenu_05_01_EG_RAD"});
